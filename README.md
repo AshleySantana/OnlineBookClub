4a9f356d84f22364a65e8e31f7e652a5a1484c5f
AshleySantana <santana02ashley@gmail.com>
2025-02-26 23:30:59 -0500

    ashley parts

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ I contributed and made to the MeetingBookClub package.
 
 
 | Team Member | Contribution |
-| -------- | ------- |
+| Ashley | I contributed to the channel package.  |
 | Manali  |   I contributed and made to the MeetingBookClub package.  |
 | Britney | I worked on both the API design and the code implementation for the Subscription Management MVC.     |
 | Stephanie    | I worked on the Channel Management MVC, and all the classes contained in the code logic    |
---
+++ src/Channels/Controller/ChannelController.java
@@ -0,0 +1,46 @@
+package Channels.Controller;
+import Channels.Model.Channel;
+import Channels.Model.Comment;
+
+public class ChannelController {
+
+    /**
+     * Discussion variables to control
+     */
+    public Channel channel;
+    public Comment comment;
+
+    /**
+     * Constructor
+     * @param channel
+     * @param comment
+     */
+    public ChannelController(Channel channel, Comment comment) {
+        this.channel = channel;
+        this.comment= comment;
+    }
+
+    /**
+     * getters and setters
+     * @return
+     */
+
+    public Channel getChannel() {
+        return channel;
+    }
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+    public Channel createChannel(){return channel;}
+    public void deleteChannel(){}
+
+    public Comment getComment() {
+        return comment;
+    }
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+    public Comment createComment(){return comment;}
+    public void deleteComment(){}
+
+}
