1febfb77a42c6382e7935b716fc44e45a393a464
Sereina <snw5334@psu.edu>
2025-04-11 11:06:27 -0400

    gui
    gui

--- src/UserAuthentication/View/UserProfileView.java
+++ src/UserAuthentication/View/UserProfileView.java
@@ -8,4 +8,10 @@ public class UserProfileView {
     private JPanel userInfoPanel;
     private JLabel usernameLabel;
     private JLabel passwordLabel;
+    private JLabel emailLabel;
+    private JLabel subStatusLabel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JTextField textField2;
+    private JLabel statusLabel;
 }
