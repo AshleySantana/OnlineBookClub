dbcd223863253c0bd7b9e76caec3710255cea286
britneywang <118696500+britneywang@users.noreply.github.com>
2025-05-01 12:38:15 -0400

    readme2

--- src/TestHarness/UnitTestCases.java
+++ src/TestHarness/UnitTestCases.java
@@ -1,4 +1,12 @@
 package TestHarness;
 
 public class UnitTestCases {
+
+    // 1. User stores payment information and receives feedback of stored information through display
+    public void userStoresPaymentInformation {
+
+    }
+
+    // 2. User adds meeting to list of meetings
+
 }
